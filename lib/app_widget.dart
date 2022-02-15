172f22123d0c73d3b10f3fb1bc7baead4cb98fa6
Julio <jucesargois@gmail.com>
2022-02-14 21:22:11 -0300

    Inserido rotas nomeadas

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,14 +1,23 @@
+import 'package:clone_nubank_dark_figma/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 import 'splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
+
   const AppWidget({Key? key}) : super(key: key);
+
+
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashPage(),
+      initialRoute: "/splash",
+      routes: {
+        "/splash" : (context) => SplashPage(),
+        "/home" : (context) =>  HomePage(),
+  }
     );
   }
 }
