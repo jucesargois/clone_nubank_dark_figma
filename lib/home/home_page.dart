25378556db24c402bb5c7bca3c36f2261465ba72
Julio <jucesargois@gmail.com>
2022-02-14 22:39:29 -0300

    Inserindo nova cor de fundo e chamadas para os Containers

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
+import 'containers/container_cartao_credito.dart';
+import 'containers/container_conta.dart';
+import 'containers/container_emprestimo.dart';
+import 'containers/container_rewards.dart';
+
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("Home Page"),
+        backgroundColor: Colors.black,
+        elevation: 0,
+        title: const Text(
+          "Olá Felipe",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: const [
+              ContainerCartaoCredito(),
+              ContainerConta(),
+              ContainerEmprestimo(),
+              ContainerRewards(),
+            
+            ],
+          ),
+        ),
       ),
     );
   }
