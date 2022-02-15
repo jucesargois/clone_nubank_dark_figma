b343a049c398fa01c0292487d8cad2df2d5a8ae2
Julio <jucesargois@gmail.com>
2022-02-14 21:21:52 -0300

    Inserida função para controle do tempo da  splash page

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -8,6 +8,18 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  void initializeSplash()async{
+    await Future.delayed(Duration(seconds: 4));
+    Navigator.pushReplacementNamed(context, "/home");
+  }
+
+   @override
+  void initState() {
+    initializeSplash();
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
